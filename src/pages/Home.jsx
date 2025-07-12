96e1547b08cd6f8c9cc369c623988e076c2853f2
Niteshbalothiya <rs5768827@gmail.com>
2025-07-13 03:42:37 +0530

    second

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,56 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
+import Chat from './Chat';
 import { RiImageAiFill } from "react-icons/ri";
 import { BiImageAdd } from "react-icons/bi";
 import { RiChat4Line } from "react-icons/ri";
 import { FaPlus } from "react-icons/fa6";
 import { FaArrowRight } from "react-icons/fa";
-import "../App.css"
+import "../App.css";
 import { dataContext } from '../context/UserContext';
-const Home = () => {
-let {startRes, setStartRes} = useContext (dataContext)
 
- async function handleSubmit (e) {
-  e.preventDefault()
-  setStartRes(true)
- }
+const Home = () => {
+  const { startRes, setStartRes } = useContext(dataContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStartRes(true);
+  };
+
   return (
     <div className='home'>
       <nav>
-        <div className="logo">
-          Smart AI
-        </div>
+        <div className="logo">Smart AI</div>
       </nav>
 
-      <div className="hero">
-        <span id='tag'>What can I help with....?</span>
-
-        <form action="" className="input-box" onSubmit={(e) =>handleSubmit()}>
-        <button id='add'><FaPlus /></button>
-        <input type="text" placeholder='As Something.....' />
-        <button id='submit'><FaArrowRight /></button>
-
-      </form>
-
-        <div className="cate">
-          <div className="upImg">
-            <BiImageAdd />
-            <span>Upload Image</span>
-          </div>
-          <div className="genImg">
-            <RiImageAiFill />
-            <span>Generate Image</span>
-          </div>
-          <div className="chat">
-            <RiChat4Line />
-            <span>Let`s Chat</span>
+      {!startRes ? (
+        <div className="hero">
+          <span id='tag'>What can I help with....?</span>
+          <div className="cate">
+            <div className="upImg">
+              <BiImageAdd />
+              <span>Upload Image</span>
+            </div>
+            <div className="genImg">
+              <RiImageAiFill />
+              <span>Generate Image</span>
+            </div>
+            <div className="chat">
+              <RiChat4Line />
+              <span>Let's Chat</span>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <Chat />
+      )}
 
-
-      
+      <form className="input-box" onSubmit={(e) => handleSubmit(e)}>
+        <button id='add' type="button"><FaPlus /></button>
+        <input type="text" placeholder='Ask Something...' />
+        <button id='submit' type="submit"><FaArrowRight /></button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
